6712b742d408adbf0f51c61a1ea8e3f2bcf36a3b
Minduck <21520730@gm.uit.edu.vn>
2022-11-27 02:08:12 +0700

    add taskscore

--- components/ShareComponents/taskboard.js
+++ components/ShareComponents/taskboard.js
@@ -97,18 +97,19 @@ export default function Taskboard({user}) {
           <div className={Style.seperator}></div>
           <p>POSITION</p>
         </div>
-
+        <p>Task score</p>
         <p>Date Assigned</p>
       </div>
       {missions &&
         missions.map((item, idx) => {
-          return (
+          return ( 
             <div>
               <div className={Style.top20_info}>
                 <div className={Style.top20_info_left}>
                   <h1><GoPrimitiveDot /></h1>
                   <p>{ `${item.location_desc}` }</p>
                 </div>
+                <p>{`${item.misson_score}`}</p>
                 <div className={Style.top20_info_right}>
                   <p >{item.mission_time.replace('T', ' ')}</p>
                   <a  
