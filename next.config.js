b3162332daf408244cc4198138abd7c870b12ee2
hao3830 <21520832@gm.uit.edu.vn>
2022-11-27 03:47:04 +0700

    deploy

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
+  basePath:'/hackathon/2022/frontend',
+  output: 'standalone',
+
 }
 
 module.exports = nextConfig
